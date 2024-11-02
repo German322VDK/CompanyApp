b80ba69aacddaf3afd5a96243fda570e5c771a12
Герман <65992547+German322VDK@users.noreply.github.com>
2024-11-02 17:03:37 +1000

    модели представления

---
+++ CompanyApp.Domain/ViewModels/EmployeeBaseViewModel.cs
@@ -0,0 +1,41 @@
+﻿using CompanyApp.Domain.Entities;
+using System.ComponentModel.DataAnnotations;
+
+namespace CompanyApp.Domain.ViewModels
+{
+    /// <summary>
+    /// Базовая модель представления для сотрудника
+    /// </summary>
+    public abstract class EmployeeBaseViewModel
+    {
+        /// <summary>
+        /// Имя сотрудника
+        /// </summary>
+        [Required]
+        public string FirstName { get; set; }
+
+        /// <summary>
+        /// Отчество сотрудника.
+        /// </summary>
+        [Required]
+        public string Patronymic { get; set; }
+
+        /// <summary>
+        /// Фамилия сотрудника.
+        /// </summary>
+        [Required]
+        public string SurName { get; set; }
+
+        /// <summary>
+        /// Должность сотрудника в организации. 
+        /// </summary>
+        [Required]
+        public Position JobTitle { get; set; }
+
+        /// <summary>
+        /// Идентификатор непосредственного руководителя сотрудника.
+        /// Значение может быть null, если у сотрудника нет руководителя.
+        /// </summary>
+        public int? LeaderId { get; set; }
+    }
+}
